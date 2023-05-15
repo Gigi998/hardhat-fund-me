583fa8995f3ed5cfd81d13e255eac4d463fb9c22
Luigi <luigi.drnasin12@gmail.com>
2023-05-15 08:26:34 +0200

    readme update

--- scripts/fund.js
+++ scripts/fund.js
@@ -1,7 +1,7 @@
 const { ethers, getNamedAccounts } = require('hardhat');
 
 async function main() {
-  const sendValue = ethers.utils.parseEther('0.1');
+  const sendValue = ethers.utils.parseEther('0.029');
   const { deployer } = await getNamedAccounts();
   const fundMe = await ethers.getContract('FundMe', deployer);
   console.log('Funding contract....');
